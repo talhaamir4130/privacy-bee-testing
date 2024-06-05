e25093e2f97d6b438d0970bce4ac2a6f82b8bd13
Talha Amir <talhaamir2014@gmail.com>
2024-06-05 16:54:02 +0000

    write tests

---
+++ tests/EmployeeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EmployeeTest extends WebTestCase
+{
+    public function testUpdateEmployee(): void
+    {
+        $client = static::createClient();
+        $updateData = [
+            'first_name' => 'Talha',
+            'middle_name' => null,
+            'last_name' => 'Amir',
+            'email' => ['talhaamir2018@gmail.com'],
+        ];
+    
+        $client->request(
+            Request::METHOD_PUT,
+            '/employees/1',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($updateData)
+        );
+    
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function testUpdateEmployeeWrongInputs(): void
+    {
+        $client = static::createClient();
+        $updateData = [
+            'first_name' => ['Talha'],
+            'middle_name' => null,
+            'last_name' => 0,
+            'email' => 12345,
+        ];
+
+        $client->request(
+            Request::METHOD_PUT,
+            '/employees/1',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($updateData)
+        );
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
+    }
+}
